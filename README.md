c9761123b0920d8e06d71910beb9df7d353ee969
brandong1 <brandon@brandonleeproductions.com>
2020-05-05 15:17:05 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# swift-trackingCOVID19
---
+++ TrackingCOVID19/CovidFetchRequest.swift
@@ -0,0 +1,9 @@
+//
+//  CovidFetchRequest.swift
+//  TrackingCOVID19
+//
+//  Created by Brandon Green on 5/5/20.
+//  Copyright © 2020 Brandon Lee Green. All rights reserved.
+//
+
+import Foundation
