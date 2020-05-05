0d9ab28c6aaa7233dc9c88f00b88df825bc11cf5
brandong1 <brandon@brandonleeproductions.com>
2020-05-05 14:22:22 -0600

    Initial Commit

---
+++ TrackingCOVID19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackingCOVID19
+//
+//  Created by Brandon Green on 5/5/20.
+//  Copyright © 2020 Brandon Lee Green. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
