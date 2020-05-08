4a21e2138282a14d30db6fd54ce4bef28ba04674
brandong1 <brandon@brandonleeproductions.com>
2020-05-07 20:20:33 -0600

    finish recent view

--- TrackingCOVID19/RecentView.swift
+++ TrackingCOVID19/RecentView.swift
@@ -11,9 +11,54 @@ import SwiftUI
 struct RecentView: View {
     
     @ObservedObject var covidFetch = CovidFetchRequest()
+    @State var searchText = ""
+    @State var isSearchVisible = false
     
     var body: some View {
-        Text("Country")
+        
+        NavigationView {
+            
+            VStack {
+                
+                if isSearchVisible {
+                    SearchView(searchText: $searchText)
+                }
+                
+                TotalDataView(totalData: covidFetch.totalData)
+                ListHeaderView()
+                
+                List {
+                    
+                    ForEach(covidFetch.allCountries.filter {
+                        self.searchText.isEmpty ? true : $0.country.lowercased().contains(self.searchText.lowercased())
+                    }, id: \.country) { countryData in
+                        
+                        NavigationLink(destination: CountryDetailView(countryData: countryData)) {
+                            
+                            CountryDataRowView(countryData: countryData)
+                        }
+                    }
+                }
+                
+            }//End of VStack
+                .navigationBarTitle("Recent Data", displayMode: .inline)
+                .navigationBarItems(trailing:
+                    
+                    Button(action: {
+                        
+                        self.isSearchVisible.toggle()
+                        
+                        if !self.isSearchVisible {
+                            self.searchText = ""
+                        }
+                        
+                    }, label: {
+                        Image(systemName: "magnifyingglass")
+                    })
+            )
+            
+        }//End of Navigation view
+        
     }
 }
 
