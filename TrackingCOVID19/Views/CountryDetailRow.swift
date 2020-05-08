8cb3fa0947bffbb190b8a5e71cb807021d0625f3
brandong1 <brandon@brandonleeproductions.com>
2020-05-07 20:21:30 -0600

    finish country detail row

---
+++ TrackingCOVID19/Views/CountryDetailRow.swift
@@ -0,0 +1,47 @@
+//
+//  CountryDetailRow.swift
+//  TrackingCOVID19
+//
+//  Created by Brandon Green on 5/7/20.
+//  Copyright © 2020 Brandon Lee Green. All rights reserved.
+//
+
+import SwiftUI
+
+struct CountryDetailRow: View {
+    
+    var number: String = "Err"
+    var name: String = "Confirmed"
+    var color: Color = .primary
+    
+    var body: some View {
+        
+        
+        VStack {
+            
+            HStack {
+                Text(self.name)
+                    .font(.body)
+                    .padding(5)
+                
+                Spacer()
+                
+                Text(self.number)
+                    .font(.subheadline)
+                    .padding(5)
+                    .foregroundColor(color)
+
+            }
+
+            Divider()
+        }
+        .padding(.leading)
+        .padding(.trailing)
+    }
+}
+
+struct CountryDetailRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CountryDetailRow()
+    }
+}
